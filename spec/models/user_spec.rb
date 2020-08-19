f74f984dfea3b19815053054a1dd4d9d2137e4d1
mohupor03 <68655084+mohupor03@users.noreply.github.com>
2020-08-19 11:49:39 +0900

    Merge develop to master (#26)
    * Modify README.md (#27)
    
    * Create master.key
    
    * Update README.md (#2)
    
    * Create key (#3)
    
    * credentials.yml.encを削除
    
    * masterkeyの作成
    
    * Update:【再】Userリクエストのテスト (#22)
    
    * Update:Userリクエストのテスト
    
    * Refactor:robocopのリファクタリング
    
    * Refactor:robocopのリファクター２
    
    * User model test (#25)
    
    * add helloに変更
    
    * Revert "add helloに変更"
    
    This reverts commit 3f1ec4a673684226feaaa3902a0797fe375d495d.
    
    * feat：userModelのTestを追加
    
    * uppdate テスト
    
    * refactor: userモデルのテストをリファクタ
    
    * Revert "refactor: userモデルのテストをリファクタ"
    
    This reverts commit a3a0b629bfd20bed059abd9558dcb3450f81c68c.
    
    * 再　refactor: userモデルのテストをリファクタ
    
    Co-authored-by: Kenta Katsumata <polyglot.software@gmail.com>
    Co-authored-by: keito-sakakibara <62434610+keito-sakakibara@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'Taro', email: 'taro@example.com') }
+
+  it 'is valid with a name, user_id, email' do
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user.name = nil
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without an email address' do
+    user.email = nil
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
 end
