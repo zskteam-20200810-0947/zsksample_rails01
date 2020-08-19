6c6a77264f391ddba1cf259c434ee3dfe819e6a1
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-19 17:55:44 +0900

    Update:Micropostモデルテストの実装

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
+require 'securerandom'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Taro', email: 'taro@example.com') }
+  let(:micropost) { Micropost.new(content: 'Post', user_id: user.id) }
+  let(:over_140) { SecureRandom.alphanumeric(141) }
+
+  it 'is valid with a content' do
+    expect(micropost).to be_valid
+  end
+
+  it 'is invalid without a content' do
+    micropost.content = nil
+    micropost.valid?
+    expect(micropost.errors[:content]).to include("can't be blank")
+  end
+
+  it 'is invalid content over 140 characters' do
+    micropost.content = over_140
+    micropost.valid?
+    expect(micropost.errors[:content]).to include('is too long (maximum is 140 characters)')
+  end
+
+  it 'is invalid without user_id' do
+    micropost.user_id = nil
+    micropost.valid?
+    expect(micropost.errors.full_messages).to include('User must exist')
+  end
 end
