d0c70516ab6102aab5955781fb9a49f998001594
Kenichiro-Ogawa <65483372+Kenichiro-Ogawa@users.noreply.github.com>
2020-08-23 16:13:38 +0900

    Merge ：developブランチをmasterへマージ (#32)
    * Modify README.md (#27)
    
    * Create master.key
    
    * Update README.md (#2)
    
    * Create key (#3)
    
    * credentials.yml.encを削除
    
    * masterkeyの作成
    
    * Update:【再】Userリクエストのテスト (#22)
    
    * Update:Userリクエストのテスト
    
    * Refactor:robocopのリファクタリング
    
    * Refactor:robocopのリファクター２
    
    * User model test (#25)
    
    * add helloに変更
    
    * Revert "add helloに変更"
    
    This reverts commit 3f1ec4a673684226feaaa3902a0797fe375d495d.
    
    * feat：userModelのTestを追加
    
    * uppdate テスト
    
    * refactor: userモデルのテストをリファクタ
    
    * Revert "refactor: userモデルのテストをリファクタ"
    
    This reverts commit a3a0b629bfd20bed059abd9558dcb3450f81c68c.
    
    * 再　refactor: userモデルのテストをリファクタ
    
    * Add:Microposts関連のファイルの追加 (#27)
    
    * Update README.md
    
    * Add:Microposts用ファイルの追加
    
    * Fix:rubocopによる修正
    
    Co-authored-by: Kenta Katsumata <polyglot.software@gmail.com>
    
    * Update: Micropostの文字数制限 (#28)
    
    * Update: Micropostモデルテストの実装 (#29)
    
    * Update:バリデーションとアソシエーションの設定
    
    * Update:Micropostモデルテストの実装
    
    * Delete:アソシエーションの削除
    
    * uppdate: micropostリクエストテスト (#30)
    
    * uppdate: micropostリクエストテスト
    
    * uppdate rubocop
    
    * refacter: micropostリクエストテスト
    
    * 再 refacter:micropostリクエストテスト
    
    * feat:アソシエーション機能の追加 (#31)
    
    * feat:アソシエーション機能の追加
    
    * refacter: association
    
    * refacter: micropostリクエストテスト
    
    * refacter: micropostモデルテスト
    
    Co-authored-by: Kenta Katsumata <polyglot.software@gmail.com>
    Co-authored-by: keito-sakakibara <62434610+keito-sakakibara@users.noreply.github.com>
    Co-authored-by: mohupor03 <68655084+mohupor03@users.noreply.github.com>

---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'securerandom'
+
+RSpec.describe Micropost, type: :model do
+  let(:user) { User.create(name: 'Taro', email: 'taro@example.com') }
+  let(:micropost) { Micropost.new(content: 'Post', user_id: user.id) }
+  let(:over_140) { SecureRandom.alphanumeric(141) }
+
+  it 'is valid with a content' do
+    expect(micropost).to be_valid
+  end
+
+  it 'is invalid without a content' do
+    micropost.content = nil
+    micropost.valid?
+    expect(micropost.errors[:content]).to include("can't be blank")
+  end
+
+  it 'is invalid content over 140 characters' do
+    micropost.content = over_140
+    micropost.valid?
+    expect(micropost.errors[:content]).to include('is too long (maximum is 140 characters)')
+  end
+end
---
+++ spec/requests/microposts_spec.rb
@@ -0,0 +1,117 @@
+require 'rails_helper'
+RSpec.describe '/microposts', type: :request do
+  let(:user) { User.create(name: 'Taro', email: 'taro@example.com') }
+  let(:micropost) { Micropost.create(content: 'sample', user_id: user.id) }
+  let(:un_micropost_params) { { content: ' ', user_id: user.id } }
+  let(:micropost_params) { { content: 'sample2', user_id: user.id } }
+
+  describe 'GET /index' do
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get microposts_url
+      expect(response).to be_successful
+    end
+
+    it 'indexアクションにリクエストすると200レスポンスが返ってくる' do
+      get microposts_url
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'GET /show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get micropost_url(micropost)
+      expect(response).to be_successful
+    end
+
+    it 'showアクションにリクエストすると200レスポンスが返ってくる' do
+      get micropost_url(micropost)
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'GET /new' do
+    it 'newアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get new_micropost_url
+      expect(response).to be_successful
+    end
+
+    it 'newアクションにリクエストすると200レスポンスが返ってくる' do
+      get new_micropost_url
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'GET /edit' do
+    it 'editアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get edit_micropost_url(micropost)
+      expect(response).to be_successful
+    end
+
+    it 'editアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get edit_micropost_url(micropost)
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'POST /create' do
+    context 'Micropostモデルへの保存に成功したとき' do
+      it 'Micropostモデルのレコードが１つ増加する' do
+        expect do
+          post microposts_url, params: { micropost: micropost_params }
+        end.to change(Micropost, :count).by(1)
+      end
+
+      it 'showにリダイレクトされる' do
+        post microposts_url, params: { micropost: micropost_params }
+        expect(response).to redirect_to(micropost_url(Micropost.last))
+      end
+    end
+
+    context 'Micropostモデルへの保存に失敗したとき' do
+      it 'Micropostレコードが増加しない' do
+        expect do
+          post microposts_url, params: { micropost: un_micropost_params }
+        end.to change(Micropost, :count).by(0)
+      end
+
+      it 'newにrenderされる' do
+        post microposts_url, params: { micropost: un_micropost_params }
+        expect(response.body).to include 'New Micropost'
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    context '投稿情報の更新に成功した時' do
+      it '投稿が更新される' do
+        patch micropost_url(micropost), params: { micropost: micropost_params }
+        micropost.reload
+        expect(micropost.content).to eq 'sample2'
+      end
+
+      it 'showにリダイレクトされる' do
+        patch micropost_url(micropost), params: { micropost: micropost_params }
+        expect(response).to redirect_to(micropost_url(micropost))
+      end
+    end
+
+    context '投稿情報の更新に失敗したとき' do
+      it '投稿編集ページ(:edit)にレンダーされる' do
+        patch micropost_url(micropost), params: { micropost: un_micropost_params }
+        expect(response.body).to include 'Editing Micropost'
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    it '投稿情報を削除するとUserレコードが１減る' do
+      micropost.save
+      expect { delete micropost_path(micropost) }.to change(Micropost, :count).by(-1)
+    end
+
+    it '投稿情報を削除するとユーザー一覧(:index)にリダイレクトされる' do
+      delete micropost_url(micropost)
+      expect(response).to redirect_to(microposts_url)
+    end
+  end
+end
